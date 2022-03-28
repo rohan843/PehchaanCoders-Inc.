f024d362b04a298c4c75533a8d78b602dc00f337
rohan843 <rohan.sharma2934@gmail.com>
2022-03-28 20:05:03 +0530

    Add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const parser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Create schemas and models here
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,9 +11,7 @@ app.use("*/css", express.static("public/css"));
 app.use("*/img", express.static("public/img"));
 app.use("*/js", express.static("public/js"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
+// Create responses to get, post etc here.
 
 app.listen(3000, () => {
     console.log("Server set up to listen on port 3000.");
